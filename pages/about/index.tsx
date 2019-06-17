0a373b1f47d060b2a4344bacfdd9176648d2c8da
Mike Donahue <mtdonahue1@gmail.com>
2019-06-16 20:45:35 -0700

    feat: logout (#3)

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
 import withAuthentication from '../../client/components/hoc/with-authentication';
 
-const About = (): any => <div>About</div>;
+const About = (): any => (
+  <div>
+    <h2>Pages</h2>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+    </ul>
+    <p>About Page</p>
+  </div>
+);
 
 export default withAuthentication(About);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
-import { PureComponent } from 'react';
+import { Component } from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
+import Cookies from 'universal-cookie';
 import withAuthentication from '@client/components/hoc/with-authentication';
 
-class Index extends PureComponent {
+class Index extends Component {
+  private logout = () => {
+    const cookies = new Cookies();
+    cookies.remove('jwt', { path: '/' });
+
+    return Router.push('/login');
+  };
+
   public render(): any {
     return (
       <div>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        Home Page
+        <h2>Pages</h2>
+        <ul>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+        <p>Home Page</p>
+        <button onClick={this.logout} type="button">
+          Logout
+        </button>
       </div>
     );
   }
